dd759478e9a8a6a98eec9933435e0a5c63434ec3
Hossam Magdy <hossam.magdy@outlook.com>
2021-06-11 16:46:12 +0200

    grrs (grep-like cli), cli-args(1.3), finish the first implementation
    https://rust-cli.github.io/book/tutorial/impl-draft.html

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,22 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+// Now you can run: `cargo run foo test.txt`
+// Or `cargo build --release` then `target/release/rust-workshop foo test.txt`
 fn main() {
     let args = Cli::from_args();
 
     println!(
-        "Args:\n- string: {}\n- file: {}",
+        "Args: {{ string: \"{}\", file: \"{}\" }}",
         args.pattern,
         args.path.as_path().to_str().unwrap()
     );
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
