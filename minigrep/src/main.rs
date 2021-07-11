2db75409867b531d8876b7b949f04be4e2ea9a25
Hossam Magdy <hossam.magdy@outlook.com>
2021-07-11 12:50:33 +0200

    Initial implementation of `minigrep`

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, fs};
+
+fn main() -> Result<(), std::io::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    let search_string = args
+        .get(1)
+        .expect("Search string is required as first argument");
+
+    let filename = args
+        .get(2)
+        .expect("File name is required as second argument");
+
+    let file_content = fs::read_to_string(filename)?;
+    let mut i = 0;
+
+    for line in file_content.lines() {
+        i = i + 1;
+        if line.contains(search_string) {
+            print!("Line #{}: {:?}\n", i, line);
+        }
+    }
+
+    Ok(())
 }
