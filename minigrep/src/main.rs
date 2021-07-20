486f2aec48a20398da83f3e3d4ce624c9597ee2c
Hossam Magdy <hossam.magdy@outlook.com>
2021-07-21 01:32:07 +0200

    refactor: write error messages to stderr, not stdout

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,15 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = minigrep::config::Config::new(&args).unwrap_or_else(|error| {
-        println!("Problem parsing arguments: {}", error);
+        eprintln!("Problem parsing arguments: {}", error);
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
