0070e542c0f14869361380c43c8a84450e9d1ba3
Hossam Magdy <hossam.magdy@outlook.com>
2021-07-20 23:20:53 +0200

    tests: add tests for Config struct

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,5 +1,6 @@
 use std::{error::Error, fs};
 
+#[derive(Debug, PartialEq)]
 pub struct Config<'a> {
     pub query: &'a String,
     pub filename: &'a String,
@@ -7,12 +8,6 @@ pub struct Config<'a> {
 
 impl<'a> Config<'a> {
     pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 2 {
-            return Err(
-                "Expected \"query\" as first argument, and \"filename\" as second argument",
-            );
-        }
-
         let query = args
             .get(1)
             .ok_or("Could not extract Search query from the first argument")?;
@@ -37,4 +32,42 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn config_new_handles_correct_args() {
+        let args = [
+            "bin".to_string(),
+            "queryStr".to_string(),
+            "fileNameStr".to_string(),
+        ];
+
+        assert_eq!(
+            Config::new(&args).unwrap(),
+            Config {
+                query: &"queryStr".to_string(),
+                filename: &"fileNameStr".to_string()
+            }
+        );
+    }
+
+    #[test]
+    fn config_new_handles_1_arg_as_error() {
+        assert_eq!(
+            Config::new(&["bin".to_string()]),
+            Err("Could not extract Search query from the first argument")
+        );
+    }
+
+    #[test]
+    fn config_new_handles_2_args_as_error() {
+        assert_eq!(
+            Config::new(&["bin".to_string(), "queryStr".to_string()]),
+            Err("Could not extract Filename from the second argument")
+        );
+    }
 }
