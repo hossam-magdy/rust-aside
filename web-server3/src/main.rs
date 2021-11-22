06ccf01a45cb96ed4631e608c362ffe61c2fc6bd
Hossam Magdy <hossam.magdy@outlook.com>
2021-11-23 00:25:32 +0100

    Web-server3 initial implementation: one-thread-per-request

---
+++ web-server3/src/main.rs
@@ -0,0 +1,43 @@
+use std::{
+    fs::read_to_string,
+    io::{Read, Write},
+    net::TcpListener,
+    thread,
+    time::Duration,
+};
+
+static ADDRESS: &str = "127.0.0.1:7878";
+fn main() {
+    let listener = TcpListener::bind(ADDRESS).unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        thread::spawn(|| {
+            handle_stream(stream);
+        });
+    }
+}
+
+fn handle_stream(mut stream: std::net::TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+
+    let buffer_str = String::from_utf8_lossy(&buffer[..]);
+    let req_line = buffer_str.lines().next().unwrap();
+    println!("{}", req_line);
+
+    let (status_line, filepath) = match req_line {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "public/index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(3));
+            ("HTTP/1.1 200 OK", "public/index.html")
+        }
+        _ => ("HTTP/1.1 200 OK", "public/404.html"),
+    };
+    let content = read_to_string(filepath).unwrap();
+    let response = format!("{}\n\n{}", status_line, content);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
