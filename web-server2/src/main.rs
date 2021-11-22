57a3ca8c28051e4a0118fa4ace2467c5a4a2cfe0
Hossam Magdy <hossam.magdy@outlook.com>
2021-11-22 12:16:46 +0100

    Fix panicking issue, create pool ince, not for every request!

--- web-server2/src/main.rs
+++ web-server2/src/main.rs
@@ -7,10 +7,10 @@ static ADDRESS: &str = "127.0.0.1:7878";
 
 fn main() {
     let tcp_listener = TcpListener::bind(ADDRESS).unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in tcp_listener.incoming() {
         let stream = stream.unwrap();
-        let pool = ThreadPool::new(4);
 
         // do we "move" in thread::spawn?
         pool.execute(|| {
